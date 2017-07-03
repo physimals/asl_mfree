2ef74c254d82bdffe17cf4cced875737fe5f2adf
Michael Chappell <michael.chappell@eng.ox.ac.uk>
2017-07-03 14:36:39 +0100

    Initial commit

---
+++ asl_mfree.cc
@@ -0,0 +1,188 @@
+/*   asl_mfree.cc 'Model-free' deconvolution for multi-TI ASL data
+
+      Michael Chappell - IBME & FMIRB Image Analysis Group
+
+      Copyright (C) 2011 University of Oxford */
+
+/*   CCOPYRIGHT   */
+
+#include <iostream>
+#include <math.h>
+#include <string>
+#include "newmatap.h"
+#include "newmatio.h"
+#include "newimage/newimageall.h"
+#include "miscmaths/miscmaths.h"
+#include "utils/tracer_plus.h"
+#include "stdlib.h"
+
+#include "readoptions.h"
+#include "asl_mfree_functions.h"
+
+using namespace Utilities;
+using namespace NEWMAT;
+using namespace NEWIMAGE;
+using namespace MISCMATHS;
+
+using namespace OXASL;
+
+int main(int argc, char *argv[])
+{
+  try {
+
+    cout << "ASL_MFREE (1.0)" << endl;
+
+    //parse command line (puts these into the log file)
+    ReadOptions& opts = ReadOptions::getInstance();
+    opts.parse_command_line(argc,argv);
+
+    cout << "Loading data" << endl;
+
+    // load data    
+    volume4D<float> data;
+    read_volume4D(data,opts.datafile.value());
+
+    // load mask
+    volume<float> mask(data.xsize(),data.ysize(),data.zsize());
+    mask.setdims(data.xdim(),data.ydim(),data.zdim());
+    read_volume(mask,opts.maskfile.value());
+
+    Matrix asldata;
+    asldata = data.matrix(mask);
+    //data.setmatrix(asldata,mask);
+    int nvox=asldata.Ncols();
+    
+    //load aif
+    volume4D<float> aif;
+    read_volume4D(aif,opts.aif.value());
+
+    
+    // select AIF based on metric
+    // load metric image if it exists
+    volume<float> metric;
+    if (opts.metric.set()) {
+      cout << "Preparing AIFs" << endl;
+      read_volume(metric,opts.metric.value());
+
+      Prepare_AIF(aif, metric, mask, opts.mthresh.value());
+      
+      //volume4D<float> aifout;
+      //aifout.setmatrix(aif,mask);
+      save_volume4D(aif,opts.outname.value()+"_aifs");
+    }
+
+    Matrix aifmtx;
+    aifmtx = aif.matrix(mask);
+
+    // do deconvolution
+    cout << "Performing deconvolution" << endl;
+    ColumnVector mag;
+    Matrix resid;
+    Deconv(asldata,aifmtx,opts.dt.value(),mag,resid);
+
+    // estimate BAT (of tissue)
+    ColumnVector batt;
+    if (opts.batout.set() | (opts.tcorrect.set() & !opts.batt.set())) {
+      cout << "Estimating BAT" << endl;
+      Estimate_onset(asldata,batt,opts.dt.value());
+
+      if (opts.batout.set()) {
+	//output the BAT image (from the tissue)
+	volume4D<float> batout;
+	batout.setmatrix(batt.AsMatrix(1,nvox),mask);
+	save_volume4D(batout,opts.outname.value()+"_bat");
+      }
+    }
+
+    // correct aif magntiude for differences in arrival time between aif and tissue 
+    ColumnVector batd;
+    if(opts.tcorrect.set()) {
+      cout << "Performing timing correction" << endl;
+      
+
+      if (opts.bata.set()) {
+	//calculate difference between tissue and AIF curves using suppled BAT images
+	volume4D<float> bat_art;
+	read_volume4D(bat_art,opts.bata.value());
+	
+	if (opts.batt.set()) {
+	  //load supplied tissue BAT
+	volume4D<float> bat_tiss;
+	read_volume4D(bat_tiss,opts.batt.value());
+	
+	batt = (bat_tiss.matrix(mask)).AsColumn();
+	}
+	
+	if (opts.metric.set()) {
+	  // correct the AIF bat image to match the AIFs where a metric image has been supplied
+	  Prepare_AIF(bat_art,metric,mask,opts.mthresh.value());
+	}
+	
+	ColumnVector bata;
+	bata = (bat_art.matrix(mask)).AsColumn();
+	
+	batd = batt-bata;
+      }
+      else {
+	//otherwise estimate BAT difference using the peak in the residue function
+	//Estimate_BAT_difference(resid,batd,opts.dt.value());
+
+	// Estiamte BAT difference using edge detection
+	ColumnVector bata;
+	Estimate_onset(aifmtx,bata,opts.dt.value());
+
+	batd = batt-bata;
+      }
+
+      for (int i=1; i<=batd.Nrows(); i++) { if (batd(i)<0.0) batd(i)=0.0; }
+      Correct_magnitude(mag,batd,opts.T1.value(),opts.dt.value(),opts.fa.value());
+    }
+
+    if(opts.std.set()) {
+      // do wild boostrapping std dev estimation for cbf
+      cout << "Performing wild bootstrapping for precision estimation" << endl;
+      ColumnVector magstd;
+      BootStrap(aifmtx, asldata, opts.dt.value(), mag, resid, opts.nwb.value(), magstd);
+
+      if (opts.tcorrect.set()) {
+	// if needed we should correct the std dev for timing discrpancies between aif and ctc
+	Correct_magnitude(magstd,batd,opts.T1.value(),opts.dt.value(),opts.fa.value());
+      }
+
+      // save it
+      volume4D<float> stdout;
+      stdout.setmatrix(magstd.AsMatrix(1,nvox),mask);
+      save_volume4D(stdout,opts.outname.value()+"_stddev");
+    }
+    
+    cout << "Saving results" << endl;
+    //output 
+    volume4D<float> residout;
+    residout.setmatrix(resid,mask);
+    save_volume4D(residout,opts.outname.value()+"_residuals");
+
+    volume4D<float> magout;
+    magout.setmatrix(mag.AsMatrix(1,nvox),mask);
+    save_volume4D(magout,opts.outname.value()+"_magntiude");
+
+    cout << "ASL_MFREE - Done!" << endl;
+
+  }
+
+catch(Exception& e)
+    {
+      cerr << endl << e.what() << endl;
+      return 1;
+    }
+  catch(X_OptionError& e)
+    {
+      cerr << endl << e.what() << endl;
+      return 1;
+    }
+
+  cout << "Done." << endl;
+
+  return 0;
+
+
+}
---
+++ readoptions.h
@@ -0,0 +1,168 @@
+/*  readoptions.h
+
+    Michael Chappell - FMRIB Image Analysis Group
+
+    Copyright (C) 2009 University of Oxford  */
+
+/*  CCOPYRIGHT  */
+
+#if !defined(ReadOptions_h)
+#define ReadOptions_h
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include "utils/options.h"
+#include "utils/log.h"
+
+using namespace Utilities;
+
+namespace OXASL {
+
+class ReadOptions {
+ public:
+  static ReadOptions& getInstance();
+  ~ReadOptions() { delete ropt; }
+  
+  Option<bool> help;
+
+  Option<string> datafile;
+  Option<string> maskfile;
+  Option<string> outname;
+  Option<string> aif;
+  Option<float> dt;
+
+  Option<string> metric;
+  Option<float> mthresh;
+
+  // timing correction
+  Option<bool> tcorrect;
+  Option<string> bata;
+  Option<string> batt;
+  Option<bool> batout;
+  Option<float> T1;
+  Option<float> fa;
+
+  // std deviation estimation
+  Option<bool> std;
+  Option<int> nwb;
+
+  void parse_command_line(int argc, char** argv);
+  
+ private:
+  ReadOptions();  
+  const ReadOptions& operator=(ReadOptions&);
+  ReadOptions(ReadOptions&);
+
+  OptionParser options; 
+      
+  static ReadOptions* ropt;
+  
+};
+
+ inline ReadOptions& ReadOptions::getInstance(){
+   if(ropt == NULL)
+     ropt = new ReadOptions();
+   
+   return *ropt;
+ }
+
+ inline ReadOptions::ReadOptions() :
+
+help(string("-h,--help"), false,
+		    string("display this message"),
+		    false, no_argument),
+   //input files
+   datafile(string("--data,--datafile"), string("data"),
+			  string("ASL data file"),
+		     true, requires_argument),  
+   maskfile(string("--mask"), string("maskfile"),
+	    string("mask"),
+	    true, requires_argument),
+   outname(string("--out"), string("asl_mfree"),
+	   string("Output directory name"),
+	   true, requires_argument),
+   aif(string("--aif"),string(""),
+       string("Arterial input functions for deconvolution (4D volume, one aif for each voxel within mask)"),
+       true,requires_argument),
+   dt(string("--dt"),1.0,
+      string("Temporal spacing in data (s)\n"),
+      true,requires_argument),
+
+   metric(string("--metric"),string(""),
+	  string("Metric image"),
+	  false,requires_argument),
+   mthresh(string("--mthresh"),0.0,
+	   string("Metric threshold\n"),
+	   false,requires_argument),
+
+   tcorrect(string("--tcorrect"),false,
+	    string("Apply correction for timing difference between AIF and tissue curve"),
+	    false,no_argument),
+   bata(string("--bata"),string(""),
+	string("arterial BAT image"),
+	false,requires_argument),
+   batt(string("--batt"),string(""),
+	string("tissue BAT image"),
+	false,requires_argument),
+   batout(string("--bat"),false,
+	  string("Estimate tissue BAT from data (and save this image)"),
+	  false,no_argument),
+   T1(string("--t1"),1.6,
+      string("T1 (of blood) value"),
+      false,requires_argument),
+   fa(string("--fa"),0.0,
+      string("Flip angle (if using LL readout)"),
+      false,requires_argument),
+   
+  
+   std(string("--std"),false,
+       string("Calculate standard deviations on perfusion values using wild bootstrapping"),
+       false,no_argument),
+   nwb(string("--nwb"),1000,
+       string("Number of permuatations for wild bootstrapping"),
+       false,requires_argument),
+   
+
+   options("asl_mfree","asl_mfree --verbose\n")
+   {
+     try {
+       options.add(help);
+
+       options.add(datafile);
+       options.add(maskfile);
+       options.add(outname);
+       options.add(aif);
+       options.add(dt);
+
+       options.add(metric);
+       options.add(mthresh);
+
+       options.add(tcorrect);
+       options.add(bata);
+       options.add(batt);
+       options.add(batout);
+       options.add(T1);
+       options.add(fa);
+       
+       options.add(std);
+       options.add(nwb);
+
+        }
+     catch(X_OptionError& e) {
+       options.usage();
+       cerr << endl << e.what() << endl;
+     } 
+     catch(std::exception &e) {
+       cerr << e.what() << endl;
+     }    
+     
+   }
+
+}
+#endif
+
+
+
