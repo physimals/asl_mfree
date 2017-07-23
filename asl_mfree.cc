21e27db5159aa8c7058a76a18302a6de44f69425
mosszhaodphil <mosszhaodphil@gmail.com>
2017-07-23 11:46:40 +0100

    formatting

--- asl_mfree.cc
+++ asl_mfree.cc
@@ -6,70 +6,63 @@
 
 /*   CCOPYRIGHT   */
 
-#include <iostream>
-#include <math.h>
-#include <string>
-#include "newmatap.h"
-#include "newmatio.h"
-#include "newimage/newimageall.h"
-#include "miscmaths/miscmaths.h"
-#include "utils/tracer_plus.h"
-#include "stdlib.h"
+#
+include<iostream> #include<math.h> #include<string> #include
+    "newmatap.h" #include "newmatio.h" #include
+    "newimage/newimageall.h" #include "miscmaths/miscmaths.h" #include
+    "utils/tracer_plus.h" #include "stdlib.h"
 
-#include "readoptions.h"
-#include "asl_mfree_functions.h"
+#
+    include "readoptions.h" #include "asl_mfree_functions.h"
 
-using namespace Utilities;
+    using namespace Utilities;
 using namespace NEWMAT;
 using namespace NEWIMAGE;
 using namespace MISCMATHS;
 
 using namespace OXASL;
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char* argv[]) {
   try {
-
-    //cout << "ASL_MFREE (1.0)" << endl;
+    // cout << "ASL_MFREE (1.0)" << endl;
     cout << "ASL_MFREE (1.1)" << endl;
 
-    //parse command line (puts these into the log file)
+    // parse command line (puts these into the log file)
     ReadOptions& opts = ReadOptions::getInstance();
-    opts.parse_command_line(argc,argv);
+    opts.parse_command_line(argc, argv);
 
     cout << "Loading data" << endl;
 
-    // load data    
+    // load data
     volume4D<float> data;
-    read_volume4D(data,opts.datafile.value());
+    read_volume4D(data, opts.datafile.value());
 
     // load mask
-    volume<float> mask(data.xsize(),data.ysize(),data.zsize());
-    mask.setdims(data.xdim(),data.ydim(),data.zdim());
-    read_volume(mask,opts.maskfile.value());
+    volume<float> mask(data.xsize(), data.ysize(), data.zsize());
+    mask.setdims(data.xdim(), data.ydim(), data.zdim());
+    read_volume(mask, opts.maskfile.value());
 
     Matrix asldata;
     asldata = data.matrix(mask);
-    //data.setmatrix(asldata,mask);
-    int nvox=asldata.Ncols();
-    
-    //load aif
-    volume4D<float> aif;
-    read_volume4D(aif,opts.aif.value());
+    // data.setmatrix(asldata,mask);
+    int nvox = asldata.Ncols();
 
-    
+    // load aif
+    volume4D<float> aif;
+    read_volume4D(aif, opts.aif.value());
+
     // select AIF based on metric
     // load metric image if it exists
     volume<float> metric;
     if (opts.metric.set()) {
       cout << "Preparing AIFs" << endl;
-      read_volume(metric,opts.metric.value());
+      read_volume(metric, opts.metric.value());
 
       Prepare_AIF(aif, metric, mask, opts.mthresh.value());
-      
-      //volume4D<float> aifout;
-      //aifout.setmatrix(aif,mask);
-      save_volume4D(aif,opts.outname.value()+"_aifs");
+
+      // volume4D<float> aifout;
+      // aifout.setmatrix(aif,mask);
+      save_volume4D(aif, opts.outname.value() + "_aifs");
     }
 
     Matrix aifmtx;
@@ -79,9 +72,11 @@ int main(int argc, char *argv[])
     // By default, it is the dt value in the input
     float ti_actual = opts.dt.value();
     // For Turbo QUASAR, we need to consider the slice shifting effects
-    if(opts.turbo_quasar.set()) {
+    if (opts.turbo_quasar.set()) {
       cout << "This is Turbo QUASAR Sequence!!!" << endl;
-      cout << "The current ASL_model_free function only supports consecutive bolus, i.e. no bolus skipping!!!" << endl;
+      cout << "The current ASL_model_free function only supports consecutive "
+              "bolus, i.e. no bolus skipping!!!"
+           << endl;
       int shift_factor = opts.shift_factor.value();
       ti_actual = ti_actual / shift_factor;
     }
@@ -90,111 +85,110 @@ int main(int argc, char *argv[])
     cout << "Performing deconvolution" << endl;
     ColumnVector mag;
     Matrix resid;
-    Deconv(asldata,aifmtx,ti_actual,mag,resid);
+    Deconv(asldata, aifmtx, ti_actual, mag, resid);
 
     // estimate BAT (of tissue)
     ColumnVector batt;
     if (opts.batout.set() | (opts.tcorrect.set() & !opts.batt.set())) {
       cout << "Estimating BAT" << endl;
-      Estimate_onset(asldata,batt,ti_actual);
+      Estimate_onset(asldata, batt, ti_actual);
 
       if (opts.batout.set()) {
-	//output the BAT image (from the tissue)
-	volume4D<float> batout;
-	batout.setmatrix(batt.AsMatrix(1,nvox),mask);
-	save_volume4D(batout,opts.outname.value()+"_bat");
+        // output the BAT image (from the tissue)
+        volume4D<float> batout;
+        batout.setmatrix(batt.AsMatrix(1, nvox), mask);
+        save_volume4D(batout, opts.outname.value() + "_bat");
       }
     }
 
-    // correct aif magntiude for differences in arrival time between aif and tissue 
+    // correct aif magntiude for differences in arrival time between aif and
+    // tissue
     ColumnVector batd;
-    if(opts.tcorrect.set()) {
+    if (opts.tcorrect.set()) {
       cout << "Performing timing correction" << endl;
-      
 
       if (opts.bata.set()) {
-	//calculate difference between tissue and AIF curves using suppled BAT images
-	volume4D<float> bat_art;
-	read_volume4D(bat_art,opts.bata.value());
-	
-	if (opts.batt.set()) {
-	  //load supplied tissue BAT
-	volume4D<float> bat_tiss;
-	read_volume4D(bat_tiss,opts.batt.value());
-	
-	batt = (bat_tiss.matrix(mask)).AsColumn();
-	}
-	
-	if (opts.metric.set()) {
-	  // correct the AIF bat image to match the AIFs where a metric image has been supplied
-	  Prepare_AIF(bat_art,metric,mask,opts.mthresh.value());
-	}
-	
-	ColumnVector bata;
-	bata = (bat_art.matrix(mask)).AsColumn();
-	
-	batd = batt-bata;
+        // calculate difference between tissue and AIF curves using suppled BAT
+        // images
+        volume4D<float> bat_art;
+        read_volume4D(bat_art, opts.bata.value());
+
+        if (opts.batt.set()) {
+          // load supplied tissue BAT
+          volume4D<float> bat_tiss;
+          read_volume4D(bat_tiss, opts.batt.value());
+
+          batt = (bat_tiss.matrix(mask)).AsColumn();
+        }
+
+        if (opts.metric.set()) {
+          // correct the AIF bat image to match the AIFs where a metric image
+          // has been supplied
+          Prepare_AIF(bat_art, metric, mask, opts.mthresh.value());
+        }
+
+        ColumnVector bata;
+        bata = (bat_art.matrix(mask)).AsColumn();
+
+        batd = batt - bata;
+      } else {
+        // otherwise estimate BAT difference using the peak in the residue
+        // function
+        // Estimate_BAT_difference(resid,batd,ti_actual);
+
+        // Estiamte BAT difference using edge detection
+        ColumnVector bata;
+        Estimate_onset(aifmtx, bata, ti_actual);
+
+        batd = batt - bata;
       }
-      else {
-	//otherwise estimate BAT difference using the peak in the residue function
-	//Estimate_BAT_difference(resid,batd,ti_actual);
 
-	// Estiamte BAT difference using edge detection
-	ColumnVector bata;
-	Estimate_onset(aifmtx,bata,ti_actual);
-
-	batd = batt-bata;
+      for (int i = 1; i <= batd.Nrows(); i++) {
+        if (batd(i) < 0.0) batd(i) = 0.0;
       }
-
-      for (int i=1; i<=batd.Nrows(); i++) { if (batd(i)<0.0) batd(i)=0.0; }
-      Correct_magnitude(mag,batd,opts.T1.value(),ti_actual,opts.fa.value());
+      Correct_magnitude(mag, batd, opts.T1.value(), ti_actual, opts.fa.value());
     }
 
-    if(opts.std.set()) {
+    if (opts.std.set()) {
       // do wild boostrapping std dev estimation for cbf
       cout << "Performing wild bootstrapping for precision estimation" << endl;
       ColumnVector magstd;
-      BootStrap(aifmtx, asldata, ti_actual, mag, resid, opts.nwb.value(), magstd);
+      BootStrap(aifmtx, asldata, ti_actual, mag, resid, opts.nwb.value(),
+                magstd);
 
       if (opts.tcorrect.set()) {
-	// if needed we should correct the std dev for timing discrpancies between aif and ctc
-	Correct_magnitude(magstd,batd,opts.T1.value(),ti_actual,opts.fa.value());
+        // if needed we should correct the std dev for timing discrpancies
+        // between aif and ctc
+        Correct_magnitude(magstd, batd, opts.T1.value(), ti_actual,
+                          opts.fa.value());
       }
 
       // save it
       volume4D<float> stdout;
-      stdout.setmatrix(magstd.AsMatrix(1,nvox),mask);
-      save_volume4D(stdout,opts.outname.value()+"_stddev");
+      stdout.setmatrix(magstd.AsMatrix(1, nvox), mask);
+      save_volume4D(stdout, opts.outname.value() + "_stddev");
     }
-    
+
     cout << "Saving results" << endl;
-    //output 
+    // output
     volume4D<float> residout;
-    residout.setmatrix(resid,mask);
-    save_volume4D(residout,opts.outname.value()+"_residuals");
+    residout.setmatrix(resid, mask);
+    save_volume4D(residout, opts.outname.value() + "_residuals");
 
     volume4D<float> magout;
-    magout.setmatrix(mag.AsMatrix(1,nvox),mask);
-    save_volume4D(magout,opts.outname.value()+"_magntiude");
+    magout.setmatrix(mag.AsMatrix(1, nvox), mask);
+    save_volume4D(magout, opts.outname.value() + "_magntiude");
 
     cout << "ASL_MFREE - Done!" << endl;
-
+  } catch (Exception& e) {
+    cerr << endl << e.what() << endl;
+    return 1;
+  } catch (X_OptionError& e) {
+    cerr << endl << e.what() << endl;
+    return 1;
   }
-
-catch(Exception& e)
-    {
-      cerr << endl << e.what() << endl;
-      return 1;
-    }
-  catch(X_OptionError& e)
-    {
-      cerr << endl << e.what() << endl;
-      return 1;
-    }
 
   cout << "Done." << endl;
 
   return 0;
-
-
 }
--- asl_mfree_functions.cc
+++ asl_mfree_functions.cc
@@ -6,10 +6,10 @@
 
 /*   CCOPYRIGHT   */
 
-#include "asl_mfree_functions.h"
+#
+include "asl_mfree_functions.h"
 
-namespace OXASL {
-
+    namespace OXASL {
   ReturnMatrix SVDdeconv(const Matrix& data, const Matrix& aif, float dt) {
     // do a singular value deconvolution of the data to get residue function
 
@@ -18,166 +18,192 @@ namespace OXASL {
     float truncfac = 0.2;
 
     // voxelwise SVD deconvolution
-    Matrix aifconv; Matrix residue(nti,nvox);
-    DiagonalMatrix S; DiagonalMatrix D; Matrix U; Matrix V;
-    for (int v=1; v<=nvox; v++) {
-      //make convolution matrix
+    Matrix aifconv;
+    Matrix residue(nti, nvox);
+    DiagonalMatrix S;
+    DiagonalMatrix D;
+    Matrix U;
+    Matrix V;
+    for (int v = 1; v <= nvox; v++) {
+      // make convolution matrix
       aifconv = dt * convmtx(aif.Column(v));
-      //SVD
-      SVD(aifconv,S,U,V);
+      // SVD
+      SVD(aifconv, S, U, V);
       // invert the singular values
       D = S.i();
       // truncate (zero all singular values below threshold)
-      for (int i=2; i<=D.Nrows(); i++) {
-	if (S(i,i) < truncfac*S(1,1)) D(i,i)=0;
+      for (int i = 2; i <= D.Nrows(); i++) {
+        if (S(i, i) < truncfac * S(1, 1)) D(i, i) = 0;
       }
       // calculate resdiue
-      residue.Column(v) = V*D*U.t()*data.Column(v);
+      residue.Column(v) = V * D * U.t() * data.Column(v);
     }
-      
-    return residue; 
+
+    return residue;
   }
 
-  ReturnMatrix convmtx(const ColumnVector& invec){
+  ReturnMatrix convmtx(const ColumnVector& invec) {
     // create a (simple) convolution matrix
 
     int nentry = invec.Nrows();
-    Matrix cmat(nentry,nentry);
-    cmat=0.0;
-    for (int i=1; i<=nentry; i++) {
-      cmat.SubMatrix(i,i,1,i) = ((invec.Rows(1,i)).Reverse()).AsRow();
+    Matrix cmat(nentry, nentry);
+    cmat = 0.0;
+    for (int i = 1; i <= nentry; i++) {
+      cmat.SubMatrix(i, i, 1, i) = ((invec.Rows(1, i)).Reverse()).AsRow();
     }
 
     return cmat;
   }
 
-  ReturnMatrix SVDdeconv_circular(const Matrix& data, const Matrix& aif, float dt) {
+  ReturnMatrix SVDdeconv_circular(const Matrix& data, const Matrix& aif,
+                                  float dt) {
     // do a singular value deconvolution of the data to get residue function
 
     int nti = data.Nrows();
     int nvox = data.Ncols();
     float truncfac = 0.2;
 
-    int nextra = floor(nti*1.2);
+    int nextra = floor(nti * 1.2);
     ColumnVector padding(nextra);
     padding = 0.0;
 
     // voxelwise SVD deconvolution
-    Matrix aifconv; Matrix residue(nti+nextra,nvox);
-    DiagonalMatrix S; DiagonalMatrix D; Matrix U; Matrix V;
-    for (int v=1; v<=nvox; v++) {
-      //make convolution matrix
+    Matrix aifconv;
+    Matrix residue(nti + nextra, nvox);
+    DiagonalMatrix S;
+    DiagonalMatrix D;
+    Matrix U;
+    Matrix V;
+    for (int v = 1; v <= nvox; v++) {
+      // make convolution matrix
       aifconv = dt * convmtx_circular(aif.Column(v) & padding);
-      //SVD
-      SVD(aifconv,S,U,V);
+      // SVD
+      SVD(aifconv, S, U, V);
       // invert the singular values
       D = S.i();
       // truncate (zero all singular values below threshold)
-      for (int i=2; i<=D.Nrows(); i++) {
-	if (S(i,i) < truncfac*S(1,1)) D(i,i)=0;
+      for (int i = 2; i <= D.Nrows(); i++) {
+        if (S(i, i) < truncfac * S(1, 1)) D(i, i) = 0;
       }
       // calculate resdiue
-      residue.Column(v) = V*D*U.t()*(data.Column(v) & padding);
+      residue.Column(v) = V * D * U.t() * (data.Column(v) & padding);
     }
-      
-    residue = residue.Rows(1,nti); // only keep the number of timepoints that we have in the input.
 
-    return residue; 
+    residue = residue.Rows(
+        1,
+        nti);  // only keep the number of timepoints that we have in the input.
+
+    return residue;
   }
 
- ReturnMatrix SVDdeconv_wu(const Matrix& data, const Matrix& aif, float dt) {
+  ReturnMatrix SVDdeconv_wu(const Matrix& data, const Matrix& aif, float dt) {
     // do a singular value deconvolution of the data to get residue function
-   // circular convolution matrix
-   // OI index
-   // after Wu MRM 2003.
+    // circular convolution matrix
+    // OI index
+    // after Wu MRM 2003.
 
     int nti = data.Nrows();
     int nvox = data.Ncols();
     float oi_thresh = 0.1;
 
-    int nextra = floor(nti*1.2);
+    int nextra = floor(nti * 1.2);
     ColumnVector padding(nextra);
     padding = 0.0;
 
     // voxelwise SVD deconvolution
-    Matrix aifconv; Matrix residue(nti+nextra,nvox);
-    ColumnVector resid(nti+nextra);
-    DiagonalMatrix S; DiagonalMatrix D; Matrix U; Matrix V;
-    for (int v=1; v<=nvox; v++) {
-      //make convolution matrix
+    Matrix aifconv;
+    Matrix residue(nti + nextra, nvox);
+    ColumnVector resid(nti + nextra);
+    DiagonalMatrix S;
+    DiagonalMatrix D;
+    Matrix U;
+    Matrix V;
+    for (int v = 1; v <= nvox; v++) {
+      // make convolution matrix
       aifconv = dt * convmtx_circular(aif.Column(v) & padding);
-      //SVD
-      SVD(aifconv,S,U,V);
+      // SVD
+      SVD(aifconv, S, U, V);
       // invert the singular values
       D = S.i();
 
-      //first try with all singular values
-      resid = V*D*U.t()*(data.Column(v) & padding);
+      // first try with all singular values
+      resid = V * D * U.t() * (data.Column(v) & padding);
       float oi;
-      oi = 1/(nti*resid.Maximum())* (resid.Rows(3,nti+nextra)-2*resid.Rows(2,nti+nextra-1)+resid.Rows(1,nti+nextra-2)).SumAbsoluteValue();
+      oi = 1 / (nti * resid.Maximum()) *
+           (resid.Rows(3, nti + nextra) - 2 * resid.Rows(2, nti + nextra - 1) +
+            resid.Rows(1, nti + nextra - 2))
+               .SumAbsoluteValue();
 
       // start removing singular values one by one until we get the OI we want
-      int i=nti+nextra;
-      while (oi>oi_thresh & i>1) {
-	D(i,i) = 0;
-	resid = V*D*U.t()*(data.Column(v) & padding);
-	oi = 1/((nti+nextra)*resid.Maximum())* (resid.Rows(3,nti+nextra)-2*resid.Rows(2,nti+nextra-1)+resid.Rows(1,nti+nextra-2)).SumAbsoluteValue();
-	i--;
+      int i = nti + nextra;
+      while (oi > oi_thresh & i > 1) {
+        D(i, i) = 0;
+        resid = V * D * U.t() * (data.Column(v) & padding);
+        oi =
+            1 / ((nti + nextra) * resid.Maximum()) *
+            (resid.Rows(3, nti + nextra) - 2 * resid.Rows(2, nti + nextra - 1) +
+             resid.Rows(1, nti + nextra - 2))
+                .SumAbsoluteValue();
+        i--;
       }
 
       residue.Column(v) = resid;
     }
 
-
-    return residue; 
+    return residue;
   }
 
-  ReturnMatrix convmtx_circular(const ColumnVector& invec){
+  ReturnMatrix convmtx_circular(const ColumnVector& invec) {
     // create a (simple) convolution matrix
 
     int nentry = invec.Nrows();
-    Matrix cmat(nentry,nentry);
-    cmat=0.0;
-    for (int i=1; i<=nentry; i++) {
-      cmat.SubMatrix(i,i,1,i) = ((invec.Rows(1,i)).Reverse()).AsRow();
-      cmat.SubMatrix(i,i,i+1,nentry) = ((invec.Rows(i+1,nentry)).Reverse()).AsRow();
+    Matrix cmat(nentry, nentry);
+    cmat = 0.0;
+    for (int i = 1; i <= nentry; i++) {
+      cmat.SubMatrix(i, i, 1, i) = ((invec.Rows(1, i)).Reverse()).AsRow();
+      cmat.SubMatrix(i, i, i + 1, nentry) =
+          ((invec.Rows(i + 1, nentry)).Reverse()).AsRow();
     }
 
     return cmat;
   }
 
-  void Deconv(const Matrix& data, const Matrix& aif, float dt, ColumnVector& mag, Matrix& resid) {
-    //perform deconvolution and output the magnitude and residue function
+  void Deconv(const Matrix& data, const Matrix& aif, float dt,
+              ColumnVector& mag, Matrix& resid) {
+    // perform deconvolution and output the magnitude and residue function
 
-    //do the deconvolution
-    resid = SVDdeconv_wu(data,aif,dt);
+    // do the deconvolution
+    resid = SVDdeconv_wu(data, aif, dt);
     // extract magntiude and residue separately
     int nvox = data.Ncols();
     mag.ReSize(nvox);
-    for (int v=1; v<=nvox; v++) {
+    for (int v = 1; v <= nvox; v++) {
       mag(v) = (resid.Column(v)).Maximum();
       resid.Column(v) /= mag(v);
     }
-
   }
 
-  void BootStrap(const Matrix& aif, const Matrix& data, float dt, const ColumnVector& mag, const Matrix& resid, int Nwb, ColumnVector& magsd) {
-    // do wild boot strapping estimation of CBF precision - returns the s.d. estimate
+  void BootStrap(const Matrix& aif, const Matrix& data, float dt,
+                 const ColumnVector& mag, const Matrix& resid, int Nwb,
+                 ColumnVector& magsd) {
+    // do wild boot strapping estimation of CBF precision - returns the s.d.
+    // estimate
     int nvox = data.Ncols();
     int ntpts = data.Nrows();
 
     // calculate the residuals
     Matrix modelfit(data);
-    Matrix aifconv; ColumnVector mfittemp;
+    Matrix aifconv;
+    ColumnVector mfittemp;
     int nextra = resid.Nrows() - aif.Nrows();
     ColumnVector padding(nextra);
     padding = 0.0;
-    for (int v=1; v<=nvox; v++) {
-      //make convolution matrix
+    for (int v = 1; v <= nvox; v++) {
+      // make convolution matrix
       aifconv = dt * convmtx_circular(aif.Column(v) & padding);
       // calculate resdiue
-      mfittemp = aifconv*resid.Column(v);
-      modelfit.Column(v) = mfittemp.Rows(1,ntpts);
+      mfittemp = aifconv * resid.Column(v);
+      modelfit.Column(v) = mfittemp.Rows(1, ntpts);
     }
     Matrix residuals(data);
     residuals = data - modelfit;
@@ -187,134 +213,162 @@ namespace OXASL {
     Matrix WBdata(data);
     Matrix estresid(resid);
     Matrix WBresiduals(residuals);
-    Matrix magdist(Nwb,nvox);
+    Matrix magdist(Nwb, nvox);
     cout << "WB step (of " << Nwb << "): ";
-    for (int b=1; b<=Nwb; b++) {
+    for (int b = 1; b <= Nwb; b++) {
       cout << b << " " << flush;
       // sample from the Rademacher distrbution
-      Radevec=1.0;
-      for (int i=1; i<=ntpts; i++) {
-	if (rand() > RAND_MAX/2) { Radevec(i)=-1.0; }
+      Radevec = 1.0;
+      for (int i = 1; i <= ntpts; i++) {
+        if (rand() > RAND_MAX / 2) {
+          Radevec(i) = -1.0;
+        }
       }
 
       // apply this to the residuals to get WB residuals
-      for (int v=1; v<=nvox; v++) {
-	WBresiduals.Column(v) = SP(residuals.Column(v),Radevec);
+      for (int v = 1; v <= nvox; v++) {
+        WBresiduals.Column(v) = SP(residuals.Column(v), Radevec);
       }
       // make the WB data
       WBdata = modelfit + WBresiduals;
 
       // do the deconvolution to get magntiude estimates
-      estresid = SVDdeconv_wu(WBdata,aif,dt);
+      estresid = SVDdeconv_wu(WBdata, aif, dt);
       // extract magntiude and residue separately
 
-      for (int v=1; v<=nvox; v++) {
-	magdist(b,v) = (estresid.Column(v)).Maximum();
+      for (int v = 1; v <= nvox; v++) {
+        magdist(b, v) = (estresid.Column(v)).Maximum();
       }
     }
-    
 
     magsd.ReSize(nvox);
     magsd = (stdev(magdist)).t();
     cout << endl << "WB done" << endl;
   }
 
-  void Prepare_AIF(volume4D<float>& aif, const volume<float>& metric, const volume<float>& mask, const float mthresh) {
-    // Prepeare the AIF by retaining only the AIF that meet our metric and filling in with the nearest good AIF if necessary
+  void Prepare_AIF(volume4D<float> & aif, const volume<float>& metric,
+                   const volume<float>& mask, const float mthresh) {
+    // Prepeare the AIF by retaining only the AIF that meet our metric and
+    // filling in with the nearest good AIF if necessary
     int nx = aif.xsize();
     int ny = aif.ysize();
     int nz = aif.zsize();
 
-    float bestdist; float dist;
-    for (int x=0; x<=nx; x++) { for (int y=0; y<=ny; y++) { for (int z=0; z<=nz; z++) {
-      // if metric is not above threshold then search for nearest voxel where it is satisfied
-	  //cout << "Voxel: " << x << " " << y << " " << z << ": " << metric(x,y,z) << endl;
-	  if ( mask(x,y,z)>0 ) {
-	  if (metric(x,y,z) < mthresh) {
-	bestdist = 1e12;
-	Matrix aifcand;
-	for (int sx=0; sx<=nx; sx++) { for (int sy=0;sy<=ny; sy++) { for (int sz=0; sz<=nz; sz++) {
-	      // cout << sx << " " << sy << " " << sz << endl;
-	      if ( (mask(x,y,z)>0) && (metric(sx,sy,sz) >= mthresh) ) {
-		dist = pow(sx-x,2.0) + pow(sy-y,2.0) + pow(sz-z,2.0); //strictly this is distance squared
-		//cout << dist << endl;
-	    if (dist < bestdist) {
-	      aifcand = aif.voxelts(sx,sy,sz); 
-	      //cout << "New cand: " << aifcand.t() << endl;
-	      bestdist=dist;
-	    }
-	    else if (dist == bestdist) {
-	      aifcand |= aif.voxelts(sx,sy,sz);
-	      //cout << aifcand.t() << endl;
-	    }
-	  }
-	    } } }
-	//cout << "AIF: " << mean(aifcand,2).t() << endl;
-	    aif.setvoxelts(mean(aifcand,2),x,y,z);
-	  }
-	  }
-	} } }
+    float bestdist;
+    float dist;
+    for (int x = 0; x <= nx; x++) {
+      for (int y = 0; y <= ny; y++) {
+        for (int z = 0; z <= nz; z++) {
+          // if metric is not above threshold then search for nearest voxel
+          // where it is satisfied
+          // cout << "Voxel: " << x << " " << y << " " << z << ": " <<
+          // metric(x,y,z) << endl;
+          if (mask(x, y, z) > 0) {
+            if (metric(x, y, z) < mthresh) {
+              bestdist = 1e12;
+              Matrix aifcand;
+              for (int sx = 0; sx <= nx; sx++) {
+                for (int sy = 0; sy <= ny; sy++) {
+                  for (int sz = 0; sz <= nz; sz++) {
+                    // cout << sx << " " << sy << " " << sz << endl;
+                    if ((mask(x, y, z) > 0) &&
+                        (metric(sx, sy, sz) >= mthresh)) {
+                      dist = pow(sx - x, 2.0) + pow(sy - y, 2.0) +
+                             pow(sz - z,
+                                 2.0);  // strictly this is distance squared
+                      // cout << dist << endl;
+                      if (dist < bestdist) {
+                        aifcand = aif.voxelts(sx, sy, sz);
+                        // cout << "New cand: " << aifcand.t() << endl;
+                        bestdist = dist;
+                      } else if (dist == bestdist) {
+                        aifcand |= aif.voxelts(sx, sy, sz);
+                        // cout << aifcand.t() << endl;
+                      }
+                    }
+                  }
+                }
+              }
+              // cout << "AIF: " << mean(aifcand,2).t() << endl;
+              aif.setvoxelts(mean(aifcand, 2), x, y, z);
+            }
+          }
+        }
+      }
     }
+  }
 
-  void Correct_magnitude(ColumnVector& mag, const ColumnVector& batd, const float T1, const float dt=0.0, const float fa=0.0) {
-    //magnitude correction to take acocutn fop differences in BAT between aif and tissue
+  void Correct_magnitude(ColumnVector & mag, const ColumnVector& batd,
+                         const float T1, const float dt = 0.0,
+                         const float fa = 0.0) {
+    // magnitude correction to take acocutn fop differences in BAT between aif
+    // and tissue
 
-    mag = SP(mag,exp( batd/T1 ));
-    if (fa>0) {
-      for (int v=1; v<=mag.Nrows(); v++) {
-	mag(v) *= 1/pow( cos(fa/180*M_PI),floor((batd(v)-1e-3)/dt) ); //the 1e-3 deals with the case where batd is a integer mul;tiple of dt
+    mag = SP(mag, exp(batd / T1));
+    if (fa > 0) {
+      for (int v = 1; v <= mag.Nrows(); v++) {
+        mag(v) *= 1 / pow(cos(fa / 180 * M_PI),
+                          floor((batd(v) - 1e-3) / dt));  // the 1e-3 deals with
+                                                          // the case where batd
+                                                          // is a integer
+                                                          // mul;tiple of dt
       }
     }
     cout << endl;
   }
 
-  void Estimate_BAT_difference(const Matrix& resid, ColumnVector& batd, const float dt) {
-    //Estimate the BAT difference between AIF and tissue curve from residue function
+  void Estimate_BAT_difference(const Matrix& resid, ColumnVector& batd,
+                               const float dt) {
+    // Estimate the BAT difference between AIF and tissue curve from residue
+    // function
 
     batd.ReSize(resid.Ncols());
 
     float magtemp;
     int battemp;
-    for (int v=1; v<=resid.Ncols(); v++) {
+    for (int v = 1; v <= resid.Ncols(); v++) {
       magtemp = (resid.Column(v)).Maximum1(battemp);
-      if (battemp > resid.Nrows()/2) {
-	battemp -= resid.Nrows();
+      if (battemp > resid.Nrows() / 2) {
+        battemp -= resid.Nrows();
       }
-      batd(v) = dt*(battemp-1);
+      batd(v) = dt * (battemp - 1);
     }
-
   }
 
-  void Estimate_onset(const Matrix& curves, ColumnVector& bate, const float dt) {
-    //Estimate the time of onset of the curve using edge detection
+  void Estimate_onset(const Matrix& curves, ColumnVector& bate,
+                      const float dt) {
+    // Estimate the time of onset of the curve using edge detection
 
-    int ntpts=curves.Nrows();
+    int ntpts = curves.Nrows();
     bate.ReSize(curves.Ncols());
 
     ColumnVector kernel(ntpts);
     kernel = 0.0;
     ColumnVector kern(7);
     kern << 0.006 << 0.061 << 0.242 << 0.383 << 0.242 << 0.061 << 0.006;
-    kernel.Rows(1,kern.Nrows()) = kern;
-    
+    kernel.Rows(1, kern.Nrows()) = kern;
+
     Matrix kernmtx;
     kernmtx = convmtx(kernel);
-    
+
     ColumnVector smoothdata(curves.Column(1));
-    ColumnVector dgrad(ntpts-1);
-    for (int v=1; v<=curves.Ncols(); v++) {
-      //apply smoothing kernel
-      smoothdata = kernmtx*curves.Column(v);
+    ColumnVector dgrad(ntpts - 1);
+    for (int v = 1; v <= curves.Ncols(); v++) {
+      // apply smoothing kernel
+      smoothdata = kernmtx * curves.Column(v);
       // take gradient (forward difference)
-      dgrad = smoothdata.Rows(2,ntpts) - smoothdata.Rows(1,ntpts-1);
-      float gthresh = 0.2*dgrad.Maximum();
-      int i=1; bool cont=true;
-      while (i<ntpts & cont) {
-	if (dgrad(i)>gthresh) cont=false;
-	else i++;
-	       }
-      //cout << i << " " ;
-      bate.Row(v) = i*dt;
+      dgrad = smoothdata.Rows(2, ntpts) - smoothdata.Rows(1, ntpts - 1);
+      float gthresh = 0.2 * dgrad.Maximum();
+      int i = 1;
+      bool cont = true;
+      while (i < ntpts & cont) {
+        if (dgrad(i) > gthresh)
+          cont = false;
+        else
+          i++;
+      }
+      // cout << i << " " ;
+      bate.Row(v) = i * dt;
     }
     cout << endl;
   }
